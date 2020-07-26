3878db2ca0515e8a4b42c70b1feb914eb59dbf9f
Denis <novikovdawork@yandex.ru>
2020-07-26 18:06:31 +0700

    Добавил логотип, добавил анимацию логотипа

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -28,85 +28,87 @@ export function Form(props) {
 		  initialValues={{email: "", password: "", Name: "", lastName: ""}}
 		  >{(props) => {
 		    return (
-		      <form data-testid="form" onSubmit={props.handleSubmit}>
-		      	<Grid item xs={12}>
-		      		<FormControl fullWidth>
-		      			<InputLabel htmlFor="email">Имя пользователя</InputLabel>
-		      			<Field
-		      				id="email" 
-		      				aria-describedby="my-helper-text" 
-		      				name="email"
-		      				type="email"
-		      				data-testid="email"
-		      				value={props.values.name}
-		      				className="offsetBottom"
-		      				onChange={props.handleChange}
-		      				component={myInput}/>
-		      		</FormControl>
+			    <form className="form" data-testid="form" onSubmit={props.handleSubmit}>
+		    		<Grid container spacing={2} fullWidth>
+			      	<Grid item xs={12}>
+			      		<FormControl fullWidth>
+			      			<InputLabel htmlFor="email">Имя пользователя</InputLabel>
+			      			<Field
+			      				id="email" 
+			      				aria-describedby="my-helper-text" 
+			      				name="email"
+			      				type="email"
+			      				data-testid="email"
+			      				value={props.values.name}
+			      				className="offsetBottom"
+			      				onChange={props.handleChange}
+			      				component={myInput}/>
+			      		</FormControl>
+			      	</Grid>
+			      	{component ? (
+			      			<>
+			      			<Grid item xs={6}>
+			      				<FormControl fullWidth>
+			      					<InputLabel htmlFor="name">Имя</InputLabel>
+			      					<Field
+			      						id="name" 
+			      						aria-describedby="my-helper-text" 
+			      						name="Name"
+			      						type="text"
+			      						data-testid="name"
+			      						value={props.values.name}
+			      						className="offsetBottom"
+			      						onChange={props.handleChange}
+			      						component={myInput}/>
+			      				</FormControl>
+			      			</Grid>
+			      			<Grid item xs={6}>
+			      				<FormControl fullWidth>
+			      					<InputLabel htmlFor="lastName">Фамилия</InputLabel>
+			      					<Field
+			      						id="lastName" 
+			      						aria-describedby="my-helper-text" 
+			      						name="lastName"
+			      						type="text"
+			      						data-testid="lastName"
+			      						value={props.values.name}
+			      						className="offsetBottom"
+			      						onChange={props.handleChange}
+			      						component={myInput}/>
+			      				</FormControl>
+			      			</Grid>
+			      			</>
+			      		) : (
+									<></>
+			      		)
+			      	}
+			      	<Grid item xs={12}>
+			      		<FormControl fullWidth>
+			      			<InputLabel htmlFor="password">Пароль</InputLabel>
+			      			<Field
+			      				id="password" 
+			      				aria-describedby="my-helper-text" 
+			      				name="password"
+			      				type="password"
+			      				data-testid="password"
+			      				value={props.values.name}
+			      				className="offsetBottom"
+			      				onChange={props.handleChange}
+			      				component={myInput}/>
+			      		</FormControl>
+			      	</Grid>
+			      	<Grid item xs={12} align="right">
+			      		<Button 
+			      		  variant="contained" 
+			      		  color="primary"
+			      		  data-testid="submit"
+			      		  type="submit"
+			      		>
+			      		  Войти
+			      		</Button>
+			      	</Grid>
 		      	</Grid>
-		      	{component ? (
-		      			<>
-		      			<Grid item xs={6}>
-		      				<FormControl fullWidth>
-		      					<InputLabel htmlFor="name">Имя</InputLabel>
-		      					<Field
-		      						id="name" 
-		      						aria-describedby="my-helper-text" 
-		      						name="Name"
-		      						type="text"
-		      						data-testid="name"
-		      						value={props.values.name}
-		      						className="offsetBottom"
-		      						onChange={props.handleChange}
-		      						component={myInput}/>
-		      				</FormControl>
-		      			</Grid>
-		      			<Grid item xs={6}>
-		      				<FormControl fullWidth>
-		      					<InputLabel htmlFor="lastName">Фамилия</InputLabel>
-		      					<Field
-		      						id="lastName" 
-		      						aria-describedby="my-helper-text" 
-		      						name="lastName"
-		      						type="text"
-		      						data-testid="lastName"
-		      						value={props.values.name}
-		      						className="offsetBottom"
-		      						onChange={props.handleChange}
-		      						component={myInput}/>
-		      				</FormControl>
-		      			</Grid>
-		      			</>
-		      		) : (
-								<></>
-		      		)
-		      	}
-		      	<Grid item xs={12}>
-		      		<FormControl fullWidth>
-		      			<InputLabel htmlFor="password">Пароль</InputLabel>
-		      			<Field
-		      				id="password" 
-		      				aria-describedby="my-helper-text" 
-		      				name="password"
-		      				type="password"
-		      				data-testid="password"
-		      				value={props.values.name}
-		      				className="offsetBottom"
-		      				onChange={props.handleChange}
-		      				component={myInput}/>
-		      		</FormControl>
-		      	</Grid>
-		      	<Grid item xs={12}>
-		      		<Button 
-		      		  variant="contained" 
-		      		  color="primary"
-		      		  data-testid="submit"
-		      		  type="submit"
-		      		>
-		      		  Войти
-		      		</Button>
-		      	</Grid>
-		      </form>
+			    </form>
 		    )
 		  }}
 		</Formik>
